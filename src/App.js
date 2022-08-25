e083dfcea366023b280ddbec32f1cd0764220905
Robson Narcizo <rob.narcizo.27@gmail.com>
2022-08-25 18:01:25 -0300

    Second commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Form from './components/Form';
+import Preview from './components/Preview';
 import './css/boot.css';
 
 const { Component } = React;
@@ -7,10 +8,13 @@ const { Component } = React;
 class App extends Component {
   render() {
     return (
-      <div className="container-form">
-        <h1>Formulário de Login</h1>
-        <Form />
-      </div>
+      <>
+        <div className="container-form">
+          <h1>Formulário de Login</h1>
+          <Form />
+        </div>
+        <Preview />
+      </>
     )
   }
 
---
+++ src/components/Preview.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import '../css/preview.css';
+
+const { Component } = React;
+
+class Preview extends Component{
+    render(){
+        return (
+            <article className="preview-container">
+                <h2>Dados Informados</h2>
+                {/* Eamil */}
+                <div></div>
+                {/* Salvar email */}
+                <div></div>
+            </article>
+        );
+    }
+}
+
+export default Preview;
